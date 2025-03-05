21a49475f40447cf7244d267b381c200ad1e0dcc
Abstractmelon <abstractmelons@gmail.com>
2025-03-04 18:16:45 -0700

    remove

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -4,10 +4,7 @@ module.exports = {
       name: "ladybuild",
       script: "pnpm",
       args: "start",
-      instances: 1,
       autorestart: true,
-      watch: false,
-      max_memory_restart: "512M",
       env: {
         NODE_ENV: "development",
       },
